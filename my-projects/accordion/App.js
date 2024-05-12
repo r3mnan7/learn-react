54c3abdc6065c34e50c59bf3ff5d9e6d106eaf57
r3mnan7 <poffmp@outlook.com>
2024-05-11 20:22:56 -0400

    initialized tip-calculator app

---
+++ my-projects/accordion/App.js
@@ -0,0 +1,78 @@
+import "./styles.css";
+import { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
+      ))}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="What's the difference between state and prop?"
+        num={22}
+        key="test 1"
+      >
+        State is internal data owned by the component, props are external data
+        owned by the parent component. Props are read-only, but receiving new
+        props causes the component to re-render. Usually when the parent's state
+        has been updated. Whenever a piece of state is passed as a prop, when
+        the state is changed both components re-render. While state is used to
+        make components interactive, props are used to give parent comopnents
+        the ability to configure child components. Props can be seen as settings
+        in child components which the parent component can manage.
+      </AccordionItem>
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, children, curOpen, onOpen }) {
+  const isOpen = num === curOpen;
+
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{children}</div>}
+    </div>
+  );
+}
